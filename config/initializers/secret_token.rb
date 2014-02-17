8f054fa376d0142a3e2bebc18b8a5c3137c30d00
Bhavek Budhia <bhavek.budhia@gmail.com>
2014-02-17 02:19:35 +0000

    first

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Forder::Application.config.secret_key_base = 'c517846e5b9580783e3f3065f58a535cb721884c4f7eac0a4e338dfd53d506aac56ef4132ef2167770f594c61f03b159304124ffb2268d06b0a14f56dbd85a0d'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Forder::Application.config.session_store :cookie_store, key: '_forder_session'
